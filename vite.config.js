53357452aa5aaeb2aca005a9d2a7b957983e07f6
gorg231 <stepanskiyegor142@gmail.com>
2025-06-19 17:00:35 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // URL вашего бэкенд-сервера
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['project-11j1.onrender.com'], // або 'all'
   },
-});
+})
