dc79d28bdbceda9d7a2c0acacc099a636d142b51
gorg231 <stepanskiyegor142@gmail.com>
2025-06-19 17:05:28 +0300

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['project-11j1.onrender.com'], // або 'all'
+    allowedHosts: ['https://project-phau.onrender.com'], // або 'all'
   },
 })
